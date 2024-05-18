1274cf92f02f2e153fb7c7818e950c9055ae0d94
Djave17 <dsancheza@uamv.edu.ni>
2024-05-18 17:05:57 -0600

    Prueba Ejercicio

--- Sesion17may/Funciones.cpp
+++ Sesion17may/Funciones.cpp
@@ -11,7 +11,8 @@ void mostrarEdades();
 
 //principal
 
-void principal(){
+void principal()
+{
     int opcion = 0; 
 
     while (opcion!= 6)
@@ -23,16 +24,61 @@ void principal(){
         case 1:
             AgregarEdad();
             break;
+        case 2: 
+            mostrarEdades(); 
+            break;
+        case 3: 
+            int mayor; 
+            cout<<"Edad mayor"<<endl;
+            for (int i = 0; i < pos; i++)
+            {
+                if (ages[i]>mayor)
+                {
+                    mayor = ages[i];
+                    cout<<"Edad mayor: "<<mayor<<endl;
+                }
+            
+            }
+            
+            system("pause");
+              
+            break; 
+        case 4:
+            int menor; 
+            cout<<"Edad menor"<<endl;
+            for (int i = 0; i < pos; i++)
+            {
+                if (ages[i]<menor)
+                {
+                    menor = ages[i];
+                }
+
+            }
+            break; 
+
+        case 5: 
+            int suma = 0; 
+            for (int i = 0; i < pos; i++)
+            {
+                suma += ages[i];
+            }
+            cout<<"Promedio de edades: "<<suma/pos<<endl;
+            system("pause");
+            break;
         case 6: 
             cout<<"Gud bai"<<endl;
-            break;
-        default:
             break;  
+        default:
+            cout<<"Opcion no valida"<<endl;
+            break; 
         }
-        
+
     }
+} 
     
-}
+        
+    
+
 //funciones 
 void menu(){
     system("cls || clear");
